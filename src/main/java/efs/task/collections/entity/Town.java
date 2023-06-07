71080a0f252c0c67a4b128c610898861f5c66116
stack-flood <134332066+stack-flood@users.noreply.github.com>
2023-06-07 23:00:50 +0200

    Update Town.java

--- src/main/java/efs/task/collections/entity/Town.java
+++ src/main/java/efs/task/collections/entity/Town.java
@@ -1,7 +1,7 @@
 package efs.task.collections.entity;
 
 import java.util.List;
-
+import java.util.Objects;
 
 public class Town {
     private String townName;
@@ -23,13 +23,16 @@ public class Town {
     //TODO implementacja metody equal porównująca obiekty Town na podstawie tylko townName.
     @Override
     public boolean equals(Object o) {
-        return true;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Town town = (Town) o;
+        return townName.equals(town.townName);
     }
 
     //TODO implementacja metody equal biorąca pod uwagę tylko townName.
     @Override
     public int hashCode() {
-        return 1;
+        return Objects.hash(townName);
     }
 
     @Override
