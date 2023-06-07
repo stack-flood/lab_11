bb9574edd4a67dc152f0678d5f6eb9488d91e905
stack-flood <134332066+stack-flood@users.noreply.github.com>
2023-06-07 23:00:24 +0200

    Update Hero.java

--- src/main/java/efs/task/collections/entity/Hero.java
+++ src/main/java/efs/task/collections/entity/Hero.java
@@ -1,5 +1,8 @@
 package efs.task.collections.entity;
 
+
+import java.util.Objects;
+
 public class Hero {
     private String name;
     private String heroClass;
@@ -20,13 +23,16 @@ public class Hero {
     //TODO implementacja metody equal porównująca obiekty Hero na podstawie pól name i heroClass.
     @Override
     public boolean equals(Object o) {
-        return true;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return name.equals(hero.name) && heroClass.equals(hero.heroClass);
     }
 
     //TODO implementacja metody equal biorąca pod uwagę name i heroClass.
     @Override
     public int hashCode() {
-        return 1;
+        return Objects.hash(name, heroClass);
     }
 
     @Override
